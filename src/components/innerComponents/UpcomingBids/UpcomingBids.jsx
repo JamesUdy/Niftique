110a8761bca33d1d6bb4eed790adf66edda0d63a
JamesUdy <udayamvad@gmail.com>
2023-09-06 08:55:35 +0530

    [wip] Feat: Added content to the upcoming Bids component

--- src/components/innerComponents/UpcomingBids/UpcomingBids.jsx
+++ src/components/innerComponents/UpcomingBids/UpcomingBids.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const UpcomingBids = () => {
   return (
-    <section className='container max-w-full my-24 border-t-2 border-[#1e0732ba]'>
-            <div className='flex flex-col space-y-4 m-2'>
-                <div>
-                    <h2 className='text-xl font-bold my-4'>Unlock the Future of Art Collection with Rare NFTs!</h2>
-                    <p>🚀 Dive into a universe of exceptional digital art and seize one-of-a-kind 🌌 NFTs to propel your collection to the forefront of innovation.</p>
+    <section className='container border-t-2 py-6 border-[#1e0732ba]'>
+            <div className='flex flex-col space-y-4 mx-auto'>
+                <div className='flex items-center space-x-4'>
+                    <h2 className='text-xl font-bold my-4'>Upcoming Bids</h2>
+                    <button className='flex group gap-3 py-2 px-4 border-2 text-[#9341db] border-[#7b2cbf] hover:bg-[#7a2cbf69] hover:border-[rgba(122,44,191,0.41)] hover:text-[#e0aaff] ease-in duration-300 rounded-full items-center'>
+                        <span><Link to='/marketplace'>Dive Deeper</Link></span>
+                    </button>
                 </div>
             </div>
     </section>
