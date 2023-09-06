a0cbba7a41ba1206ccdf7c4a0fa620e5e3a60c3e
JamesUdy <udayamvad@gmail.com>
2023-09-06 10:33:59 +0530

    Ui: Made NFTSection in herosection bigger in large devices

--- src/components/innerComponents/HeroSection/HeroSection.jsx
+++ src/components/innerComponents/HeroSection/HeroSection.jsx
@@ -7,7 +7,7 @@ import { Create, Explore } from '../../../assets';
 
 const HeroSection = () => {
   return (
-    <section className='container max-w-full space-y-16 lg:space-y-0 mt-12 mb-24 justify-between flex flex-col lg:flex-row'>
+    <section className='container max-w-full space-y-16 lg:space-y-0 lg:mt-12 lg:mb-24 justify-between flex flex-col lg:flex-row'>
             <div className='flex flex-col justify-center lg:w-1/2 space-y-4 m-2'>
                 <h2 className='text-4xl font-bold my-4'>Unlock the Future of Art Collection with Rare NFTs!</h2>
                 <p>🚀 Dive into a universe of exceptional digital art and seize one-of-a-kind 🌌 NFTs to propel your collection to the forefront of innovation.</p>
