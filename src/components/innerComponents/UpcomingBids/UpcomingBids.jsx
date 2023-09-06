8507013335e883e8d15b5751501c5c5558858dcf
JamesUdy <udayamvad@gmail.com>
2023-09-06 08:32:55 +0530

    [wip] Feat: Added UpcomingBids to the home section

---
+++ src/components/innerComponents/UpcomingBids/UpcomingBids.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UpcomingBids = () => {
+  return (
+    <section className='container max-w-full my-24 border-t-2 border-[#1e0732ba]'>
+            <div className='flex flex-col space-y-4 m-2'>
+                <div>
+                    <h2 className='text-xl font-bold my-4'>Unlock the Future of Art Collection with Rare NFTs!</h2>
+                    <p>🚀 Dive into a universe of exceptional digital art and seize one-of-a-kind 🌌 NFTs to propel your collection to the forefront of innovation.</p>
+                </div>
+            </div>
+    </section>
+  )
+};
+
+export default UpcomingBids;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import HeroSection from '../components/innerComponents/HeroSection/HeroSection';
+import UpcomingBids from '../components/innerComponents/UpcomingBids/UpcomingBids';
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <UpcomingBids />
     </>
   )
 };
