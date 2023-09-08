b428a2e3353ce424dd1c76c0c2ceea8f84e4e098
JamesUdy <udayamvad@gmail.com>
2023-09-08 08:46:46 +0530

    Feat: Added autoplay option for the slider component

--- src/components/innerComponents/TrendingCreations/NFTCards.jsx
+++ src/components/innerComponents/TrendingCreations/NFTCards.jsx
@@ -1,80 +1,24 @@
-// import React from 'react';
-
-// import NFTCardDetails from './NFTCardDetails';
-
-// import Slider from 'react-slick';
-
-// import dataset from './NFTData';
-
-// const sliderConfig = {
-//   slidesToShow: 4,
-//   slidesToScroll: 4,
-//   autoplay: false,
-//   autoplaySpeed: 1000,
-//   speed: 500,
-//   arrows: true,
-//   dots: true,
-//   responsive: [
-//     {
-//       breakpoint: 1024,
-//       sliderConfig: {
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//         infinite: true,
-//         dots: true
-//       }
-//     },
-//     {
-//       breakpoint: 600,
-//       sliderConfig: {
-//         slidesToShow: 2,
-//         slidesToScroll: 2
-//       }
-//     },
-//     {
-//       breakpoint: 480,
-//       sliderConfig: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1
-//       }
-//     }
-//   ]
-// }
-
-// const NFTCards = () => {
-//   return (
-//     <section className='px-24'>
-//       <Slider {...sliderConfig}>
-//         {dataset.map((data) => {
-//           return (
-//             <NFTCardDetails data={data} />
-//           )
-//         })}
-//       </Slider>
-//     </section>
-//   )
-// };
-
-// export default NFTCards;
-
 import React from 'react';
+
 import Slider from 'react-slick';
+
 import NFTCardDetails from './NFTCardDetails';
 import dataset from './NFTData';
 
 const NFTCards = () => {
   const sliderSettings = {
     dots: true,
-    infinite: false,
-    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    speed: 1500,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1280,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         }
@@ -83,7 +27,7 @@ const NFTCards = () => {
         breakpoint: 900,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         }
