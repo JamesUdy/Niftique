e88f6dbbc399afea8ac4c7931266bf226182423a
JamesUdy <udayamvad@gmail.com>
2023-09-08 19:16:27 +0530

    Ui: Changed NFTCard bg color

--- src/components/innerComponents/TrendingCreations/NFTCardDetails.jsx
+++ src/components/innerComponents/TrendingCreations/NFTCardDetails.jsx
@@ -7,7 +7,7 @@ const NFTCardDetails = (props) => {
   const { data } = props;
 
   return (
-    <section className='mx-3 sm:mx-6 p-1 h-[460px] my-8 group flex flex-col justify-between bg-[#100624c7] ring-2 ring-[#23153f] rounded-xl'>
+    <section className='mx-3 sm:mx-6 p-1 h-[460px] my-8 group flex flex-col justify-between bg-[#0f0421bd] ring-2 ring-[#23153f] rounded-xl'>
       <div className="rounded-xl overflow-hidden m-1">
         <img src={data.img} alt={data.userName} className='w-full h-full transform group-hover:scale-125 ease-in-out transition-transform duration-500' /></div>
         <div className='w-full flex flex-col space-y-1 px-3 py-1'>
@@ -19,7 +19,7 @@ const NFTCardDetails = (props) => {
         </div>
         <div className='px-2 py-2 mb-4 flex justify-between items-center'>
           <div className='flex items-center space-x-2'>
-            <img src={data.userDp} alt="User Profile Photo" className='w-8 h-8 rounded-full ring-2 ring-blue-950' />
+            <img src={data.userDp} alt="User Profile Photo" className='w-8 h-8 rounded-full ring-2 ring-[#3e207b]' />
             <div className='flex flex-col'>
               <span className='text-xs w320:text-[10px] w360:text-xs text-slate-300'>created By</span>
               <span className='text-[14px] w320:text-[12px] w360:text-[14px]'>{data.createdBy}</span>
