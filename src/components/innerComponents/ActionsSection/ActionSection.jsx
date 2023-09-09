f234cb44cc091ac9bc011224526160432f43585f
JamesUdy <udayamvad@gmail.com>
2023-09-09 22:16:50 +0530

    [wip] Feat: Added ActionSection to the homepage

---
+++ src/components/innerComponents/ActionsSection/ActionSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ActionSection = () => {
+  return (
+    <section className='container mx-auto max-w-full space-y-16 lg:space-y-0 lg:mt-12 lg:mb-24 px-2 lg:px-10 sm:px-6 flex justify-center'>
+        <div className='ring-2 w-3/4 ring-[#3b165c] px-2 lg:px-10 sm:px-6 rounded-md'>
+            <span>ActionSection</span>
+        </div>
+    </section>
+  )
+};
+
+export default ActionSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import HeroSection from '../components/innerComponents/HeroSection/HeroSection';
 import TrendingCreations from '../components/innerComponents/TrendingCreations/TrendingCreations';
+import ActionSection from '../components/innerComponents/ActionsSection/ActionSection';
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <TrendingCreations />
+      <ActionSection />
     </>
   )
 };
