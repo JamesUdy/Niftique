0f97107bcbc556ac0bdb9678d5e1aa55c2b85502
JamesUdy <udayamvad@gmail.com>
2023-09-09 12:59:44 +0530

    Refactor: Moved images inside NFTSection into seperate file

---
+++ src/components/innerComponents/HeroSection/NFTSectionImg.js
@@ -0,0 +1,20 @@
+const NFTImg = [
+    {
+        id: 1,
+        img: 'https://i.seadn.io/gae/ZIUe5fh9zue5g_0JgetTbF7xLxX_s9L5pzqhlK6i_KenUzC1jNlhoX6odaxsXDnmVeQ9RxlA5zoXTx8QaLq2IQStKT_ajCTgUZUoJmc?auto=format&dpr=1&w=256',
+    },
+    {
+        id: 2,
+        img: 'https://i.seadn.io/gae/qAB9EmlxxzdzzI-IESJf7MXCST9A-rKGZcYIKUxmyzR4g_mBMmDF2uARO6tjUa9KfDpXyTVjJEZCIOi1D6imy-FyZUL4NfhMVrQn?auto=format&dpr=1&w=256',
+    },
+    {
+        id: 3,
+        img: 'https://i.seadn.io/gcs/files/d4427e107afdaba13a404f522dbfaf50.gif?auto=format&dpr=1&w=256',
+    },
+    {
+        id: 4,
+        img: 'https://i.seadn.io/gae/SygdTBiSbzKa1bwb6zW-1v_4PwqFvUP1p9mT66DrBIwXMyYXP9g8iFGgR06aJQdnPBHZKTa3dAvqK5Jeb7CHxMpk4-VWa_c-JPcmTA?auto=format&dpr=1&w=1000',
+    },
+];
+
+export default NFTImg;
