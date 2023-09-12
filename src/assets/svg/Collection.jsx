bd34a43110c5217e33a54ca64ee46e306b87f2f7
JamesUdy <udayamvad@gmail.com>
2023-09-12 22:43:59 +0530

    Refactor: Updated Collections SVG file

--- src/assets/svg/Collection.jsx
+++ src/assets/svg/Collection.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Collection = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className='w-8 h-8' viewBox="0 0 24 24">
-        <path fill="none" stroke='#641197' className='group-hover:stroke-[#732c9f]  ease-in duration-200' strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 11H5m14 0a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2m14 0V9a2 2 0 0 0-2-2M5 11V9a2 2 0 0 1 2-2m0 0V5a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2M7 7h10"/>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className='w-8 h-8 group-hover:stroke-[#732c9f]  ease-in duration-200' stroke-width="1.5" stroke="#641197">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
     </svg>
   )
 };
