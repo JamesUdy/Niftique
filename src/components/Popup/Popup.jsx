57dc00330b5c1c75c5b78905bb61771ccc72ff88
JamesUdy <udayamvad@gmail.com>
2023-10-30 21:17:02 +0530

    ui: made popup responsive

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Popup = ({onClose, onConfirm}) => {
   return (
-    <div className="fixed w-1/2 mx-auto inset-0 flex items-center justify-center z-50">
-      <div className="bg-slate-900 p-6 rounded-lg shadow-lg shadow-slate-950 font-semibold text-justify">
-        <p className="text-lg text-gray-300 mb-4 font-mono">
+    <div className="fixed w-2/3 sm:w-1/2 mx-auto inset-0 flex items-center justify-center z-50">
+      <div className="bg-slate-900 p-6 rounded-lg shadow-lg shadow-slate-950 font-semibold tracking-tighter text-justify">
+        <p className="text-xs w360:text-sm sm:text-lg text-gray-300 mb-4 font-mono">
             🚧 This website is currently under development, and there may be unfinished components or work in progress. 🛠️ We appreciate your understanding and patience. If you wish to proceed, click "OK". 👍
         </p>
         <div className="flex justify-end">
           <button
             onClick={onClose}
-            className="mr-2 px-4 py-2 text-gray-100 border border-gray-300 rounded ease-in duration-200 hover:bg-gray-200 hover:text-gray-900 font-mono"
+            className="mr-2 px-4 py-2 text-xs w360:text-sm sm:text-lg text-gray-100 border border-gray-300 rounded ease-in duration-200 hover:bg-gray-200 hover:text-gray-900 font-mono"
           >
             Cancel
           </button>
           <button
             onClick={onConfirm}
-            className="px-4 py-2 bg-blue-600 text-white rounded ease-in duration-200 hover:bg-blue-800 font-mono"
+            className="px-4 py-2 text-xs w360:text-sm sm:text-lg bg-blue-600 text-white rounded ease-in duration-200 hover:bg-blue-800 font-mono"
           >
             OK
           </button>
