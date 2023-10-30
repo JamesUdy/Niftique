90eaf5268ceb6c85732d407b24b070bfa5d7cdac
JamesUdy <udayamvad@gmail.com>
2023-10-30 21:12:05 +0530

    ui: style popup comp

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 const Popup = ({onClose, onConfirm}) => {
   return (
     <div className="fixed w-1/2 mx-auto inset-0 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg shadow-black">
-        <p className="text-lg text-gray-700 mb-4">
-            🚧 This website is currently under development, and there may be unfinished components or work in progress. 🛠️ We appreciate your understanding and patience. If you wish to proceed, click "OK." 👍
+      <div className="bg-slate-900 p-6 rounded-lg shadow-lg shadow-slate-950 font-semibold text-justify">
+        <p className="text-lg text-gray-300 mb-4 font-mono">
+            🚧 This website is currently under development, and there may be unfinished components or work in progress. 🛠️ We appreciate your understanding and patience. If you wish to proceed, click "OK". 👍
         </p>
         <div className="flex justify-end">
           <button
             onClick={onClose}
-            className="mr-2 px-4 py-2 text-gray-600 border border-gray-300 rounded hover:bg-gray-100"
+            className="mr-2 px-4 py-2 text-gray-100 border border-gray-300 rounded ease-in duration-200 hover:bg-gray-200 hover:text-gray-900 font-mono"
           >
             Cancel
           </button>
           <button
             onClick={onConfirm}
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            className="px-4 py-2 bg-blue-600 text-white rounded ease-in duration-200 hover:bg-blue-800 font-mono"
           >
             OK
           </button>
